25eed9d62fad19d9342c85024327974b5bbfa5f8
Kenneth Mathari <mathari580@gmail.com>
2024-12-08 18:02:31 +0300

    configure ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,23 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+        verbose.set(true)
+        android.set(true)
+        outputToConsole.set(true)
+        outputColorName.set("RED")
+        disabledRules.set(setOf("import-ordering"))
+        filter {
+            exclude("**/generated/**")
+            include("**/kotlin/**")
+        }
+    }
 }
